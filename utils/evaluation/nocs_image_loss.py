e0ebe1e78dac297c7ab9b8a73bd802379f5f5cf0
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-09-25 17:25:28 -0500

    Typo fix. nocs l2 loss.

--- utils/evaluation/nocs_image_loss.py
+++ utils/evaluation/nocs_image_loss.py
@@ -9,7 +9,7 @@ def l2_nocs_image_loss(pred_nocs, gt_nocs, pred_mask, device='cpu'):
     '''
     pred_nocs = pred_nocs.to(device)
     gt_nocs   = gt_nocs.to(device)
-    pred_mask = pred_mask.to(device)
+    pred_mask = pred_mask.to(device).bool()
     pred      = pred_nocs * pred_mask
     gt        = gt_nocs[None] * pred_mask
     return torch.mean(torch.sqrt((pred - gt)**2)).item()
