a01f1e746705489e95cba5137e6659f512863c10
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-10-20 18:35:58 -0500

    Fixes rate setter.

--- utils/dummy_rate_setter.py
+++ utils/dummy_rate_setter.py
@@ -1,12 +1,13 @@
 class DummyRateSetter(object):
-    def __init__(self, rates=[1.0], inflections=[-1]):
+    def __init__(self, rates=[1.0], inflections=[]):
+        assert len(rates) > 0, "Must have at least one rate."
         self.rates = rates
         self.inflections = inflections
         self._count = 0
         self._i = 0
 
     def __call__(self):
-        if self._count > self.inflections[self._i] and self.inflections[self._i] != -1:
+        if len(self.inflections) > (self._i+1) and self._count > self.inflections[self._i]:
             self._i += 1
         self._count += 1
         return self.rates[self._i]
