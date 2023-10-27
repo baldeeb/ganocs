61c62c71dacc5fceb0d58c6b92cc2e4a39ab2e7c
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-10-27 10:16:27 -0500

    Adds sampler to balance num samples.

--- models/discriminators/util.py
+++ models/discriminators/util.py
@@ -11,3 +11,16 @@ def accuracy(value:torch.Tensor, real:bool):
         acc = torch.sum(value < 0.5).item() / (len(value) + 1e-8)
     return acc
 
+
+def sample_balancer(a:torch.Tensor, b:torch.Tensor, dim:int=0):
+    '''Given two batches of data, make sure they both
+    have the same amount of samples by randomly sampling
+    from the one with excess'''
+    len_a, len_b  = a.shape[dim],  b.shape[dim]
+    def select(x:torch.Tensor, n:int):
+        return x.index_select(dim, torch.randperm(n))
+    if len_a > len_b:
+        a = select(a, b.shape[dim])
+    elif len_a < len_b:
+        b = select(b, a.shape[dim])
+    return a, b
